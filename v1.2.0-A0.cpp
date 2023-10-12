9fd86c198d677afa304dcb53e530b278b81f81e7
Aidan Ingram <112199474+Aidan-Ingram@users.noreply.github.com>
2023-10-11 20:13:06 -0500

    Created pre-alpha v1.2.0-A0 and uploaded source

---
+++ v1.2.0-A0.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+//Code that gets a name and number, and prints them out
+int main() 
+{
+    std::cout << "Hello! Welcome to the Pre-Alpha program.\n";
+
+    int numin = 0;
+    std::cout << "Enter a number: ";
+    std::cin >> numin;
+
+    std::cout << "Your number is " << numin << std::endl;
+}
